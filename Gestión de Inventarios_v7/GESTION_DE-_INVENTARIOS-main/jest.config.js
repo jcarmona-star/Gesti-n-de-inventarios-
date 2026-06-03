4c183e43f5eb9df7f82427154dd383c6e0a60e23
alexander <jalexandercarmona@gmail.com>
2026-06-02 21:01:35 -0500

    Delete Gestión de Inventarios_v7/GESTION_DE-_INVENTARIOS-main directory

--- Gestión de Inventarios_v7/GESTION_DE-_INVENTARIOS-main/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  testMatch: ['**/__tests__/**/*.test.js'],
-  collectCoverageFrom: ['server.js'],
-  verbose: true
-};
